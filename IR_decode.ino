2547a786fa565e4d3feaa892b8e2ec477c462fe4
Marek Kopciuszynski <mkopciuszynski@gmail.com>
2024-02-18 17:55:43 +0100

    [cleanup] Remove white spaces.

--- IR_decode.ino
+++ IR_decode.ino
@@ -7,7 +7,6 @@
 #define BAUD 115200
 #include <util/setbaud.h>
 
-
 #define cbi(sfr, bit) ((sfr) &= ~_BV(bit))
 #define sbi(sfr, bit) ((sfr) |= _BV(bit))
 
@@ -16,15 +15,11 @@ volatile uint8_t ir_decode = 0;
 volatile uint8_t ir_ready = 0;
 volatile uint8_t ir_count = 0;
 
-//=================================================================
-//        USART Serial
-//=================================================================
 void initUSART(void) {
     UBRR0H = UBRRH_VALUE;
     UBRR0L = UBRRL_VALUE;
     UCSR0A |= _BV(U2X0);
-    // Enable USART trans/rec
-    UCSR0B = _BV(TXEN0) | _BV(RXEN0);
+    UCSR0B = _BV(TXEN0)  | _BV(RXEN0);
     UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);  // 8 data bits, 1 stop bit
 }
 void transmitByte(uint8_t data) {
@@ -41,27 +36,23 @@ void printString(const char myString[]) {
 
 int main(void) {
     initUSART();
-    printString("Start\n");
 
     sbi(PORTD, PD2);
     cli();
-    EICRA = _BV(ISC00);  // INT0 any change
+    EICRA = _BV(ISC00);              // INT0 any change
     EIMSK = _BV(INT0);
-    // set timer 2 to 300 ms
-    OCR2A = 149;                      // 300 ms
-
+    OCR2A = 149;                     // 300 ms
     TCCR2A = _BV(WGM21);             // CTC
     TCCR2B = _BV(CS21) | _BV(CS20);  // prescaler 32
     TIMSK2 = _BV(OCIE2A);
     TCNT2 = 0;
-
     sei();
+    printString("Start. Waiting for IR signal.\n");
 
+    // main loop
     while (1) {
-
-
         if (ir_ready) {
-            ir_ready = 0;
+            ir_ready=0;
             cli();
             if (ir_decode == 0xFF)
                 transmitByte(10);  // new line
@@ -71,17 +62,16 @@ int main(void) {
                 transmitByte(49);  //1
             sei();
         }
-
     }
 }
 
 
 ISR(INT0_vect) {
-    if (PIND & _BV(PD2)) {  //high - start counting
+    if (PIND & _BV(PD2)) {      // high - start conversion
         TCNT2 = 0;
         ir_ready = 0;
         ir_decode = 0;
-    } else {  // low - stop and sent result
+    } else {                    // low - stop, results ready to serial transmition
         ir_ready = 1;
     }
 }
